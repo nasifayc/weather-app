6ec4acdccabc86146ce61d235e145e0af3ac6b0d
Nasifay <nasifayc11@gmail.com>
2024-05-31 20:16:17 +0300

    unit testing

--- weather_app/test/business_logic/cubits/cubit/weather_cubit_test.dart
+++ weather_app/test/business_logic/cubits/cubit/weather_cubit_test.dart
@@ -1,3 +1,4 @@
+import 'package:bloc_test/bloc_test.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:weather_app/business_logic/cubits/cubit/weather_cubit.dart';
 import 'package:weather_app/data/models/weather_model.dart';
